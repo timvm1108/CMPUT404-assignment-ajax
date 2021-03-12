38e26a644d8c945325459a3ce416120232d6cdca
Tim van Maren <timvanmaren@hotmail.com>
2021-03-12 13:55:11 -0700

    Implemented Server and AJAX

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ Contributors / Licensing
 
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
 
+Collaborated with Fraser Redford, fredford
 
--- server.py
+++ server.py
@@ -74,27 +74,36 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect('static/index.html', 302)
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    body = flask_post_json()
+    myWorld.set(entity, body)
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    # If a POST request is sent to world, overwrite the world with the world sent in the body.
+    if request.method == 'POST':
+        body = flask_post_json()
+        myWorld.clear()
+        for entity in body.keys():
+            myWorld.set(entity, body[entity])
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return json.dumps(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
